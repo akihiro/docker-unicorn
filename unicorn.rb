fb32403d0fa61a8dd3e15527421fec53113c1f22
Hiroaki Mizuguchi <hiroaki-m@iij.ad.jp>
2017-01-13 14:45:39 +0900

    Don't create pid file

--- unicorn.rb
+++ unicorn.rb
@@ -1,12 +1,9 @@
 process   = ENV['PROCESS'] || 1
-dir       = ENV['DIR']     || '/unicorn'
 listen    = ENV['LISTEN']  || '/unicorn/unicorn.sock'
 
 worker_processes process
 
 listen listen
-
-pid "#{dir}/unicorn.pid"
 
 stdout_path '/var/log/unicorn/stdout.log'
 stderr_path '/var/log/unicorn/stderr.log'
